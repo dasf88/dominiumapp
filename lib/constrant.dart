6bae3e5695d584fa5505562d10af2c85cb29c92a
Dany Salas <dany.a.salas@gmail.com>
2020-10-14 14:17:08 -0400

    Agregando - Imagenes e iconos Home Splash Screen

---
+++ lib/constrant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0x083b66);
+const kSecondaryColor = Color(0x1869ab);
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,42 @@
+import 'package:dominium_app/constrant.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dominium',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Login'),
-    );
+   return MaterialApp(
+     title: "Dominium",
+     theme: ThemeData(
+       primaryColor: kPrimaryColor,
+     ),
+     home: HomePage(),
+   );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      backgroundColor: kPrimaryColor,
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('Imagenes/DominiunLogo.png'),
+                  )
+                ),
+
+          ))
+        ],
+      )
     );
   }
 }
+
+
