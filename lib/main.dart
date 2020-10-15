fd15960ac68c18e98f522839cbbd1e68fc914561
Dany <dany_salas@jce.do>
2020-10-15 06:57:12 -0400

    Pantalla Login y Splash Aplicado el diseño de colores y Theme.

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     title: "Dominium",
-     theme: ThemeData(
-       primaryColor: kPrimaryColor,
-       textTheme: TextTheme(
-         headline5: TextStyle(
-           color: Colors.white,
-           fontWeight: FontWeight.bold,
-         ),
-         button: TextStyle(
-           color: Colors.white,
-         )
-       ),
-       inputDecorationTheme: InputDecorationTheme(
-         enabledBorder: UnderlineInputBorder(
-           borderSide: BorderSide(
-             color: Colors.white.withOpacity(.4),
-           ),
-         ),
-       ),
-     ),
-     home: HomePage(),
-   );
+    return MaterialApp(
+      title: "Dominium",
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        textTheme: TextTheme(
+            headline5: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            button: TextStyle(
+              color: Colors.white,
+            )),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white.withOpacity(.4),
+            ),
+          ),
+        ),
+      ),
+      home: HomePage(),
+    );
   }
 }
 
@@ -38,89 +36,84 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: Column(
-        children: [
-          Expanded(
+        backgroundColor: kPrimaryColor,
+        body: Column(
+          children: [
+            Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('imagenes/DominiumLogo_AllWhite.png'),
-                  )
-                ),
-          ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "CONDOMINIUM MANAGEMENT\n",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline5
-                          .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                    TextSpan(
-                      text: "A smart way to manage your residence business.",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                      )
-                    )
-                  ]
-                )
-                ),
-                FittedBox(
-                  child: GestureDetector(
-    onTap: () {
-      Navigator.push(context, MaterialPageRoute(builder: (context){
-        return SignInPage();
-      },
-      ),
-      );
-    },
-    child:  Padding(
-        padding: const EdgeInsets.only(bottom: 90),
-        child:
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 26, vertical: 16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: Colors.white,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Login",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: kPrimaryColor,
-                  fontSize: 20,
-                ),
+                    image: DecorationImage(
+                  image: AssetImage('imagenes/DominiumLogo_AllWhite.png'),
+                )),
               ),
-              Icon(Icons.arrow_forward,
-                color: kPrimaryColor,
-                size: 30,),
-            ],
-          ),
-        )
-    )
-
-    )
-
-                )
-              ],
             ),
-          )
-        ],
-      )
-    );
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(children: [
+                        TextSpan(
+                          text: "CONDOMINIUM MANAGEMENT\n",
+                          style: Theme.of(context).textTheme.headline5.copyWith(
+                              fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        TextSpan(
+                            text:
+                                "A smart way to manage your residence business.",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ))
+                      ])),
+                  FittedBox(
+                      child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return SignInPage();
+                                },
+                              ),
+                            );
+                          },
+                          child: Padding(
+                              padding: const EdgeInsets.only(bottom: 90),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 26, vertical: 16),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: Colors.white,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: kPrimaryColor,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward,
+                                      color: kPrimaryColor,
+                                      size: 30,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                          ),
+                      ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
-
-
--- lib/signin_page.dart
+++ lib/signin_page.dart
@@ -5,90 +5,103 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body:
-        Column(
+        backgroundColor: kPrimaryColor,
+        body: Column(
           children: [
             Expanded(
-              child:
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage("imagenes/DominiumIcon_White.png"),
-                    ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("imagenes/DominiumIcon_White.png"),
                   ),
                 ),
+              ),
             ),
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-    child:
-    Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                  Text("Sign In",
-                    style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  Text("Sign Up",
-                    style: Theme.of(context).textTheme.button.copyWith(fontWeight: FontWeight.bold),
-                  ),
-              ]
-    ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 5, left: 5,),
-                        child: Icon(Icons.email,
-                        color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "E-mail",
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Sign In",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Sign Up",
+                            style: Theme.of(context)
+                                .textTheme
+                                .button
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 5,
+                            left: 5,
+                          ),
+                          child: Icon(
+                            Icons.email,
+                            color: Colors.white,
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 5, left: 5,),
-                        child: Icon(Icons.lock,
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "E-mail",
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 5,
+                            left: 5,
+                          ),
+                          child: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 90),
+                      child: Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
                           color: Colors.white,
                         ),
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Password",
-                          ),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: kPrimaryColor,
                         ),
                       ),
-                    ],
-                  ),
-                  Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 90),
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                      ),
-                      child: Icon(Icons.arrow_forward,
-                      color: kPrimaryColor,),
-
-                    ),
-                  )
-                ],
-              ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
-        )
-    );
+        ));
   }
 }
